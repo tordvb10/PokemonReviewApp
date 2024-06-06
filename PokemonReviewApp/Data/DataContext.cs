85337150dcb5ac5ce6b6239506e2d27c4a26e3a1
tordvb10 <tordvb10.kodehode@gmail.com>
2024-06-06 13:11:05 +0200

    haskey

---
+++ PokemonReviewApp/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Models;
+namespace PokemonReviewApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories {  get; set; }
+        public DbSet<Country> Countries {  get; set; }
+        public DbSet<Owner> Owners {  get; set; }
+        public DbSet<Pokemon> Pokemons {  get; set; }
+        public DbSet<PokemonCategory> PokemonCategories {  get; set; }
+        public DbSet<PokemonOwner> PokemonOwners {  get; set; }
+        public DbSet<Review> Reviews {  get; set; }
+        public DbSet<Reviewer> Reviewers {  get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PokemonCategory>()
+                .HasKey(pc => new { pc.PokemonId, pc.CategoryId });
+        }
+    }
+}
--- PokemonReviewApp/Models/Pokemon.cs
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -8,5 +8,6 @@
         public ICollection<Review> Reviews { get; set; }
         public ICollection<PokemonOwner> PokemonOwners { get; set; }
         public ICollection<PokemonCategory> PokemonCategories { get; set; }
+
     }
 }
