8004fba56de5372fa844db89cfea2dbc769c20ce
tordvb10 <tordvb10.kodehode@gmail.com>
2024-06-06 12:02:34 +0200

    modelsssss

--- PokemonReviewApp/Models/PokemonOwner.cs
+++ PokemonReviewApp/Models/PokemonOwner.cs
@@ -3,5 +3,9 @@
     public class PokemonOwner
     {
         public int PokemonId { get; set; }
+        public int OwnerId { get; set; }
+        public Pokemon Pokemon { get; set; }
+        public Owner Owner { get; set; }
+
     }
 }
